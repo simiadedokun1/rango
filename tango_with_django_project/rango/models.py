58311a0e94215b2aad29ad61bdc31da2c27556e6
Simi Adedokun <3007649a@student.gla.ac.uk>
2026-02-11 14:59:26 +0000

    Chapter 9: added UserProfile model and forms

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,4 +1,5 @@
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
 from django.db import models
@@ -28,3 +29,11 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
